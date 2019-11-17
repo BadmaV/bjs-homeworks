837dc1ab09d98d9e6123c96744a81fb6729e00b2
Badma Andraev <andraev@ya.ru>
2019-11-17 16:10:15 +0300

    task 2

--- wrappers-for-primitives/task.js
+++ wrappers-for-primitives/task.js
@@ -13,6 +13,7 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
 
     // код для задачи №1 писать здесь
     //return totalAmount;
+    'use strict';
 
     function isNumber(item) { 
       if (Number.isFinite(Number(item))) {
