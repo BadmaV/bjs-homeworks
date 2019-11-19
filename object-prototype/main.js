dfb13efd558b1a919c7c43a640e58f678a8e7f53
Badma Andraev <andraev@ya.ru>
2019-11-19 16:50:26 +0300

    initial commit

--- object-prototype/main.js
+++ object-prototype/main.js
@@ -8,6 +8,10 @@ function initCheckBirthday() {
 
 function checkBirthday(birthday) {
     // код для задачи №1 писать здесь
+    let birthDate = new Date(birthday.slice(0,4), birthday.slice(5,7)-1, birthday.slice(8,10));
+    let age = (Date.now() - +birthDate) / ((31536000000*3+31622400000)/4);
+
+    return (age > 18);
 }
 
 function initPrintAnimalSound() {
@@ -22,6 +26,16 @@ function initPrintAnimalSound() {
 
 function getAnimalSound(animal) {
     // код для задачи №2 писать здесь
+
+    if (typeof animal == "undefined") {
+       return undefined;
+    }
+        
+    if('sound' in animal) {
+        return animal.sound;
+    } else {
+        return undefined;
+    }
 }
 
 function initCalculateStatement() {
@@ -36,4 +50,9 @@ function initCalculateStatement() {
 
 function getAverageMark(marks) {
     // код для задачи №3 писать здесь
+    let sum = 0;
+    for (let i = 0; i < marks.length; i++) {
+      sum += parseInt(marks[i], 10);
+    }
+    return Math.round(sum / marks.length);
 }
