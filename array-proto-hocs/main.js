1a50eba5b0520949e884301af1ecd7d67be75534
Badma Andraev <andraev@ya.ru>
2019-11-26 19:28:55 +0300

    initial commit

---
+++ array-proto-hocs/main.js
@@ -0,0 +1,41 @@
+function compareArrays(arr1, arr2) {
+	return (arr1.length === arr2.length) &&	(arr1.every((item, index) => item === arr2[index]));
+}
+
+//compareArrays([8, 1, 2], [8, 1, 2]);
+
+function memoize(fn, limit) {
+  const results = [];
+  return (...args) => {
+    let result = results.find(item => compareArrays(item.args, Array.from(args)));
+
+
+    if (result) {
+      console.log("Результат берется из памяти");
+      console.log(result.result);
+      return result.result;
+    } else {
+      let result = fn( ...args);
+      results.push({
+        args: Array.from(args),
+        result: result,
+      });
+    }
+
+    if (results.length > limit) {
+			results.shift();
+		}
+
+    console.log("Результат берется не из памяти");
+    console.log(results);
+    return results;
+  }
+}
+
+const sum = (a, b) => a + b;
+
+const mSum = memoize(sum, 2);
+mSum( 3, 4 );
+mSum( 1, 3 ); 
+mSum( 3, 4 );
+mSum( 1, 3 ); 
