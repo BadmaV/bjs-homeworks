c583c4f3aa608b508699174eaf3bfce5c9d318a9
Badma Andraev <andraev@ya.ru>
2019-12-03 18:51:55 +0300

    amendment

--- async/main.js
+++ async/main.js
@@ -1,9 +1,15 @@
-let wakeUpTime = "8:00";
+let wakeUpTime = "08:00";
 let goToBedTime = "22:00";
 
 let setDailyRhythm = (wakeUpTime, bedTime) => {
 setInterval(checkTimeEvening, 1000);
 setInterval(checkTimeMorning, 1000);
+
+const goodMorning = () => alert('Доброе утро!');
+const goToBed = () => alert('Пора спать!')
+
+checkTimeMorning = setAlarm('08:00', goodMorning);
+checkTimeEvening = setAlarm('22:42', goToBed);
 }
 
 let setAlarm = (time, callback) => {
@@ -11,19 +17,12 @@ let setAlarm = (time, callback) => {
   let currentDate = new Date();
   let hrs = currentDate.getHours();
   let min = currentDate.getMinutes();
-  let currentTime= hrs + ":" + min;
+  let currentTime = hrs + ":" + min;
   
   if (time == currentTime) {
     return callback();
   }
   }
 }
-
-const goodMorning = () => alert('Доброе утро!');
-const goToBed = () => alert('Пора спать!')
-
-checkTimeMorning = setAlarm('8:00', goodMorning);
-checkTimeEvening = setAlarm('22:42', goToBed);
-
 
 setDailyRhythm(wakeUpTime, goToBedTime);
